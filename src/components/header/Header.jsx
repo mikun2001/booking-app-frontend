d7a31de026f9d6fe00364c1b1cfa2fbcab248ac8
mikun2001 <abhilashdash2023@gmail.com>
2024-01-23 07:58:26 +0530

    search context added and minor fixes on list

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,16 +9,17 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css";
 import { DateRange } from "react-date-range";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
+import { SearchContext } from "../../context/SearchContext";
 
 const Header = ({ type }) => {
 	const [destination, setDestination] = useState("");
 	const [openDate, setOpenDate] = useState(false);
-	const [date, setDate] = useState([
+	const [dates, setDates] = useState([
 		{
 			startDate: new Date(),
 			endDate: new Date(),
@@ -43,9 +44,14 @@ const Header = ({ type }) => {
 			};
 		});
 	};
+	const { dispatch } = useContext(SearchContext);
 
 	const handleSearch = () => {
-		navigate("/hotels", { state: { destination, date, options } });
+		dispatch({
+			type: "NEW_SEARCH",
+			payload: { destination, dates, options },
+		});
+		navigate("/hotels", { state: { destination, dates, options } });
 	};
 
 	return (
@@ -114,20 +120,20 @@ const Header = ({ type }) => {
 								<span
 									onClick={() => setOpenDate(!openDate)}
 									className="headerSearchText">{`${format(
-									date[0].startDate,
+									dates[0].startDate,
 									"MM/dd/yyyy"
 								)} to ${format(
-									date[0].endDate,
+									dates[0].endDate,
 									"MM/dd/yyyy"
 								)}`}</span>
 								{openDate && (
 									<DateRange
 										editableDateInputs={true}
 										onChange={(item) =>
-											setDate([item.selection])
+											setDates([item.selection])
 										}
 										moveRangeOnFirstSelection={false}
-										ranges={date}
+										ranges={dates}
 										className="date"
 										minDate={new Date()}
 									/>
--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -13,6 +13,7 @@ import {
 import { useContext, useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import { useLocation, useNavigate } from "react-router-dom";
+import { SearchContext } from "../../context/SearchContext";
 
 const Hotel = () => {
 	const location = useLocation();
@@ -21,6 +22,17 @@ const Hotel = () => {
 	const [open, setOpen] = useState(false);
 
 	const { data, loading, error } = useFetch(`/hotels/find/${id}`);
+
+	const { dates, options } = useContext(SearchContext);
+
+	const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+	function dayDifference(date1, date2) {
+		const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+		const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
+		return diffDays;
+	}
+
+	const days = dayDifference(dates[0].endDate, dates[0].startDate);
 
 	const handleOpen = (i) => {
 		setSlideNumber(i);
@@ -107,24 +119,17 @@ const Hotel = () => {
 								<p className="hotelDesc">{data.desc}</p>
 							</div>
 							<div className="hotelDetailsPrice">
-								<h1>Perfect for a {9}-night stay!</h1>
+								<h1>Perfect for a {days}-night stay!</h1>
 								<span>
 									Located in the real heart of Krakow, this
 									property has an excellent location score of
 									9.8!
 								</span>
 								<h2>
-									<b>
-										$
-										{9 *
-											data.cheapestPrice *
-											9}
-									</b>{" "}
-									({9} nights)
+									<b>${days * data.cheapestPrice * days}</b> ({days}{" "}
+									nights)
 								</h2>
-								<button >
-									Reserve or Book Now!
-								</button>
+								<button>Reserve or Book Now!</button>
 							</div>
 						</div>
 					</div>
