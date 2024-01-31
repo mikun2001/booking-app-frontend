ff842f591b4b8c2669e6e6cd58c5d9fd345fc731
mikun2001 <abhilashdash2023@gmail.com>
2024-01-31 10:26:43 +0530

    home citywise fetch  fixes

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,3 +1,4 @@
+import { Link, useNavigate } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import Loader from "../loader/Loader";
 import "./featured.css";
@@ -6,49 +7,44 @@ const Featured = () => {
 	const { data, loading, error } = useFetch(
 		"/hotels/countByCity?cities=cuttack,bbsr,salepur"
 	);
-	// console.log("data=>", data)
+	console.log(data.length);
 
+	const navigate = useNavigate();
+	// console.log("data=>", data)
+	const handleClick = (city) => {
+		navigate(`/hotels/city/${city}`);
+	};
+	const photos = {
+		cuttack:
+			"https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=",
+		bbsr: "https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o=",
+		salepur:
+			"https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o=",
+	};
 	return (
 		<div className="featured">
-			{loading ? (
+			{loading || data.length == 0 ? (
 				// "Loading please wait"
 				<Loader />
 			) : (
 				<>
-					<div className="featuredItem">
-						<img
-							src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-							alt=""
-							className="featuredImg"
-						/>
-						<div className="featuredTitles">
-							<h1>Cuttack</h1>
-							<h2>{data[0]} properties</h2>
-						</div>
-					</div>
-
-					<div className="featuredItem">
-						<img
-							src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
-							alt=""
-							className="featuredImg"
-						/>
-						<div className="featuredTitles">
-							<h1>BBSR</h1>
-							<h2>{data[1]} properties</h2>
-						</div>
-					</div>
-					<div className="featuredItem">
-						<img
-							src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
-							alt=""
-							className="featuredImg"
-						/>
-						<div className="featuredTitles">
-							<h1>Salepur</h1>
-							<h2>{data[2]} properties</h2>
-						</div>
-					</div>
+					{data.map((element) => {
+						return (
+							<div className="featuredItem">
+								<Link to={`/hotels/city/${element._id}`}>
+									<img
+										src={photos[element._id]}
+										alt=""
+										className="featuredImg"
+									/>
+								</Link>
+								<div className="featuredTitles">
+									<h1>{element._id}</h1>
+									<h2>{element.total} properties</h2>
+								</div>
+							</div>
+						);
+					})}
 				</>
 			)}
 		</div>
--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -3,13 +3,13 @@ import "./loader.css";
 const Loader = () => {
 	return (
 		<div className="divLoader">
-			<span className="spanLoader1">↓L</span>
-			<span className="spanLoader2">↓ O</span>
-			<span className="spanLoader3">↓ A</span>
-			<span className="spanLoader4">↓ D</span>
-			<span className="spanLoader5">↓ I</span>
-			<span className="spanLoader6">↓ N</span>
-			<span className="spanLoader7">↓ G</span>
+			<span className="spanLoader1">↓ L </span>
+			<span className="spanLoader2">↓ O </span>
+			<span className="spanLoader3">↓ A </span>
+			<span className="spanLoader4">↓ D </span>
+			<span className="spanLoader5">↓ I </span>
+			<span className="spanLoader6">↓ N </span>
+			<span className="spanLoader7">↓ G </span>
 			<span className="spanLoader8">↓</span>
 		</div>
 	);
